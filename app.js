2b7875a2a16a34d68bce77c03f0d3c8a3907a8af
yolixtly <yolixtly_13@hotmail.com>
2016-07-27 14:49:39 -0400

    almost finished

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(function() {
 
     //STEP 1 : get the data from API youtube 
     function getData() {
-        var url = 'https://www.googleapis.com/youtube/v3/search';
+        var url = 'https://www.googleapis.com/youtube/v3/search/';
         var params = {
             part: 'snippet',
             key: 'AIzaSyCQQNC20CuEMQVuJSe-NVyZ9NXZHUbWlgc',
@@ -12,25 +12,46 @@ $(function() {
         };
 
         $.getJSON(url, params, function(data) {
-            console.log(data);
+        	//STEP 2 : Analize the API Data  
+        	// alert('hi');
+           console.log(data);
+            showData(data);
         });
     }
 
-    getData();
-    
-    function showData(){
+    // on click call this function
+  
+getData();
+
+    // STEP 3 : Show/render it to HTML 
+    function showData(result){
         var html = '';
-        $.each(data, function(index, value) {
-            html = '<img src="' + value.snippet.thumbnails.high.url + '" class="col-xs-12 col-md-4 img-responsive">'
-            $("#resultsContainer").append(html);
+        console.log('out of loop: ' + result.items[0].snippet.thumbnails.high.url);
+        $.each(result, function(value, key) {
+        	console.log('inside loop: ' + value.snippet.thumbnails.high.url);
+            // html += '<img src="' + value.snippet.thumbnails.high.url  + '" class="col-xs-12 col-md-4 img-responsive">'
+            // $("#resultsContainer").append(html);
         });
+        // console.log(html);
     }
+
+    //STEP 4 : Create an Event handler 
+    //that on click calls our data and Show Data functions
+
+   $('#submit').on('click', function(e){
+   		e.preventDefault;
+   		var searchTerm = $('#searchTerm').val();
+   		alert(searchTerm);
+   		// getData(searchTerm);
+   });
 
 });
 
 
-//STEP 2 : Analize the API Data  
+//STEP 5 : Work with design
 
-// STEP 3 : Show/render it to HTML 
 
-//STEP 4 : Work with design
+
+
+
+
