c780ff0756d8e47b5d3223d08c0396a40059ee65
0e0 <ejersonb@gmail.com>
2016-07-27 11:48:35 -0500

    Second Commit

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 //PLAN of steps to do 
 $(function() {
 
-	//STEP 1 : get the data from API youtube 
-    function getData(searchTerm){
-    	var url = 'https://www.googleapis.com/youtube/v3/search';
-    	var params = {
-        part: 'snippet',
-        key: 'AIzaSyCQQNC20CuEMQVuJSe-NVyZ9NXZHUbWlgc',
-        q: 'cats', //searchTerm here
-        maxResults: 10
-    };
+    //STEP 1 : get the data from API youtube 
+    function getData() {
+        var url = 'https://www.googleapis.com/youtube/v3/search';
+        var params = {
+            part: 'snippet',
+            key: 'AIzaSyCQQNC20CuEMQVuJSe-NVyZ9NXZHUbWlgc',
+            q: 'cats', //searchTerm here
+            maxResults: 10
+        };
 
-    $.getJSON(url, params, function(data) {
-        console.log(data);
-    });
+        $.getJSON(url, params, function(data) {
+            console.log(data);
+        });
     }
 
+    getData();
+    
     function showData(){
-    	
+        var html = '';
+        $.each(data, function(index, value) {
+            html = '<img src="' + value.snippet.thumbnails.high.url + '" class="col-xs-12 col-md-4 img-responsive">'
+            $("#resultsContainer").append(html);
+        });
     }
 
 });
 
 
-
-//STEP 2 : Analize the API Data 
+//STEP 2 : Analize the API Data  
 
 // STEP 3 : Show/render it to HTML 
 
